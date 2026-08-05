155865622c92dace092ce128ed7b4b2fe17732fc
Bruno <bruno.mestres@poscontrole.com.br>
2026-08-05 10:20:07 -0300

    feat: add guards jwt

---
+++ src/auth/guard/session.guard.ts
@@ -0,0 +1,32 @@
+import {
+	CanActivate,
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+} from "@nestjs/common";
+import { AuthService } from "../service/auth.service";
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+	constructor(private readonly authService: AuthService) {}
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const req = context.switchToHttp().getRequest();
+		const sessionToken = req.headers["x-session-token"];
+
+		if (!sessionToken) {
+			throw new UnauthorizedException("Session token required");
+		}
+
+		try {
+			const session = await this.authService.validateSessionToken(sessionToken);
+
+			if (!session.valid || !session.user) {
+				throw new UnauthorizedException("Invalid session token");
+			}
+			req.user = session.user;
+			return true;
+		} catch (error) {
+			throw new UnauthorizedException("Invalid session token");
+		}
+	}
+}
